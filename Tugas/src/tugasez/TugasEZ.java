f4948f62ff4f419bc84edf9bbc0a5a784ff208c4
Ilyssr <103397786+sryna19@users.noreply.github.com>
2024-06-01 23:36:48 +0700

    Add files via upload

---
+++ Tugas/src/tugasez/TugasEZ.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasez;
+
+/**
+ *
+ * @author USER
+ */
+public class TugasEZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Hal1().setVisible(true); 
+    }
+    
+}
